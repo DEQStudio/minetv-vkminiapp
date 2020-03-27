e12e853d23f5f221469329452cd1ad6e47aad97c
Aram Virabyan <googlaram200@gmail.com>
2020-03-27 23:22:56 +0300

    Update Home.js

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -12,16 +12,17 @@ const Home = ({ id, go, fetchedUser }) => (
 	<Panel id={id}>
 		<PanelHeader>MineTV</PanelHeader>
 		{fetchedUser &&
-		<Group title="MineTV official player iframe">
-			<div style='position: relative; padding-bottom: 56%; height: 0;'><iframe src='https://app.viloud.tv/player/embed/channel/3e6502a2759a7c8f59c081980bedecd0?autoplay=1&volume=1&controls=1&title=0&share=0&open_playlist=0&random=0' style='position: absolute; top: 0; left: 0; width: 100%; height: 100%;' frameborder='0' allow='autoplay' allowfullscreen></iframe></div>
+		<Group title="User Data Fetched with VK Bridge">
+			<Cell
+				before={fetchedUser.photo_200 ? <Avatar src={fetchedUser.photo_200}/> : null}
+				description={fetchedUser.city && fetchedUser.city.title ? fetchedUser.city.title : ''}
+			>
+				{`${fetchedUser.first_name} ${fetchedUser.last_name}`}
+			</Cell>
 		</Group>}
 
 		<Group title="Navigation Example">
-			<Div>
-				<Button size="xl" level="2" onClick={go} data-to="persik">
-					Show me the Persik, please
-				</Button>
-			</Div>
+			<div style='position: relative; padding-bottom: 56%; height: 0;'><iframe src='https://app.viloud.tv/player/embed/channel/3e6502a2759a7c8f59c081980bedecd0?autoplay=1&volume=1&controls=1&title=0&share=0&open_playlist=0&random=0' style='position: absolute; top: 0; left: 0; width: 100%; height: 100%;' frameborder='0' allow='autoplay' allowfullscreen></iframe></div>
 		</Group>
 	</Panel>
 );
