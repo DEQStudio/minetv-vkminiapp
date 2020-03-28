76b66531dc2068066f9a542ea8a2d7e502924cd6
Aram Virabyan <googlaram200@gmail.com>
2020-03-28 17:58:58 +0300

    fff chat support

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -37,7 +37,66 @@ const Home = ({ id, go, fetchedUser }) => (
 			</Div>
 			
 		</Group>
-	</Panel>
+		<View popout={popout} activePanel='home' >
+                <Panel id={id} >
+                    <PanelHeader
+                    left={<Button data-to='statistics' level="3" onClick={()=>{onRefresh()}}>
+                        <Icon36Replay width={24} height={24} className="MyBlock"/></Button>}>
+                        Premium Чат
+                    </PanelHeader>
+                    <PullToRefresh onRefresh={()=>onRefresh()} isFetching={fetching}>
+                        <List style={{ paddingBottom:70, backgroundColor: "var(--background_page)"}}>
+                            { DataMes == null ?
+                               null :  getSize(DataMes.list) === 0 ?
+                                    <Placeholder
+                                    icon={<Icon28MessagesOutline className="MyBlock" />}
+                                    header="Пока никто не написал  сообщений">
+                                    Будьте первыми и напишите сообщение
+                                </Placeholder> : DataMes.map(item=>
+                                    <Group shadow={0} style={{padding:"0px",margin:"10px"}}  >
+                                        <Link href={`https://vk.com/id${item.vk_id}`} target="_top" className="link" >
+                                            <Div className="Profile">
+                                                <Avatar  src={item.img} size={25}/>
+                                                <p style={{marginLeft:"10px"}}>{`${item.name} `}</p>
+                                                <p style={{color:"gray"}}> ● {convertTimestamp(item.date)}</p>
+                                            </Div>
+                                        </Link>
+                                        <Div style={{paddingTop:"0px", marginLeft:"20px",paddingLeft:"0px",paddingRight:"0px" }}>
+                                            {`${item.message}`}
+                                        </Div>
+                                    </Group>)
+                            }
+                        </List>
+                    </PullToRefresh>
+                    {fetchedUser != null ? openAlert===0 ? getUser():null :null}
+
+                    <FixedLayout vertical="bottom" >
+                        <Group >
+                            <Div >
+                                <FormLayout style={{padding:0}}>
+                                <Input onChange={handleChange} top={`Сообщение ● стоимость - ${pGpG*message.length}`} type={"text"} hidden="Текст" />
+                            </FormLayout>
+                                <Button size="xl" onClick={()=>{
+                                    if(message.length > 1){
+                                        if(message.length < 250){
+                                            WakeUpPay();
+                                        }else{
+                                            OpenError("Длина сообщения должна быть меньше 250 символов")
+                                        }
+                                    }else{
+                                        OpenError("Длина сообщения минимум 2 символа")
+                                    }
+                                    // OpenError("Минимум должно быть 2 символа");
+                                    // setModel('writeMessage');
+                                }} data-to={WRITE_MESSAGE_MODAL}  level="commerce">
+                                    Написать в чат
+                                </Button>
+                            </Div>
+                        </Group>
+                    </FixedLayout>
+                    {snackbar}
+                </Panel>
+            </View>
 );
   openBase () {
     if (this.state.snackbar) return;
