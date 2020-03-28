9b597859b7a381060a77a4950fce8a157015fdec
Aram Virabyan <googlaram200@gmail.com>
2020-03-28 14:26:38 +0300

    10500 update of mediaplyer.

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -28,7 +28,7 @@ const Home = ({ id, go, fetchedUser }) => (
 					Show me the Persik, please
 				</Button>
 				<Iframe url="https://app.viloud.tv/player/embed/channel/3e6502a2759a7c8f59c081980bedecd0?autoplay=1&volume=1&controls=1&title=0&share=0&open_playlist=0&random=0"
-        width=80%
+        width="80%"
         height="400px"
         id="myId"
         className="myClassname"
@@ -36,8 +36,8 @@ const Home = ({ id, go, fetchedUser }) => (
         position="relative"/>
 			</Div>
 			<Iframe url="https://app.viloud.tv/player/embed/channel/3e6502a2759a7c8f59c081980bedecd0?autoplay=1&volume=1&controls=1&title=0&share=0&open_playlist=0&random=0"
-        width=80%
-        height="400px"
+        width="80%"
+        height="100%"
         id="myId"
         className="myClassname"
         display="initial"
