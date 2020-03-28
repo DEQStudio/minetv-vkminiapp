bee8470819a8b86a36f655966fc6ced846695e59
Aram Virabyan <googlaram200@gmail.com>
2020-03-28 17:40:52 +0300

    trying to add notifications

--- src/panels/Home.js
+++ src/panels/Home.js
@@ -24,7 +24,7 @@ const Home = ({ id, go, fetchedUser }) => (
 
 		<Group title="Navigation Example">
 			<Div>
-				<Button size="xl" level="2" onClick={go} data-to="persik">
+				<Button size="xl" level="2" onClick={go} data-to="openBase">
 					Show me the Persik, please
 				</Button>
 				<Iframe url="https://app.viloud.tv/player/embed/channel/3e6502a2759a7c8f59c081980bedecd0?autoplay=1&volume=1&controls=1&title=0&share=0&open_playlist=0&random=0"
@@ -39,6 +39,18 @@ const Home = ({ id, go, fetchedUser }) => (
 		</Group>
 	</Panel>
 );
+  openBase () {
+    if (this.state.snackbar) return;
+    this.setState({ snackbar:
+      <Snackbar
+        layout="vertical"
+        onClose={() => this.setState({ snackbar: null })}
+        before={<Avatar size={24} style={blueBackground}><Icon16Done fill="#fff" width={14} height={14} /></Avatar>}
+      >
+        Уведомления о подкастах включены
+      </Snackbar>
+    });
+  }
 
 Home.propTypes = {
 	id: PropTypes.string.isRequired,
